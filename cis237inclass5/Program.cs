e1894f82daed80c9b2d933e8037fbbfc8aa4a247
Anthony Aernie <aaernie7528@mail.kvcc.edu>
2017-03-22 19:34:35 -0400

    database connection

--- cis237inclass5/Program.cs
+++ cis237inclass5/Program.cs
@@ -10,6 +10,19 @@ namespace cis237inclass5
     {
         static void Main(string[] args)
         {
+
+            //make new instance of the cars collection
+            CarsAAernieEntities carsAAernieEntities = new CarsAAernieEntities();
+
+            //List out all of the cars in the table
+            Console.WriteLine("Print the list");
+
+            foreach(Car car in carsAAernieEntities.Cars)
+            {
+                Console.WriteLine(car.id + " " + car.make + " " + car.model);
+            }
+
+
         }
     }
 }
